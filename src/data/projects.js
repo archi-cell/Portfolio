66957a72508c8c719093d70b10436588f01e3d27
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-02-06 12:10:40 +0530

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,58 @@
+const projects = [
+    {
+        id: "eventopia",
+        title: "Eventopia",
+        description:
+            "A full-featured event booking platform with venue selection, cart, checkout, reviews and live streaming.",
+        tech: ["React", "Node.js", "JavaScript", "Bootstrap","Express","MongoDB"],
+        Links:"Live:  https:/eventopia-backend-eight.vercel.app/",
+        github: "Github Link : https://github.com/archi-cell/eventopia-backend.git"
+        ,
+        prob3lem:
+            "Event planning involves multiple fragmented steps such as venue booking, catering, scheduling and payment, often handled on separate platforms.",
+
+        solution:
+            "Eventopia centralizes the entire event booking process into a single intuitive platform with real-time booking, cart and checkout features.",
+
+        features: [
+            "Venue & catering selection",
+            "Event calendar booking",
+            "Cart & checkout system",
+            "Live stream integration",
+            "Reviews & ratings",
+        ],
+    },
+    {
+        id: "vibechat",
+        title: "Vibechat",
+        description:
+            "Real-time chat application with authentication, Socket.IO messaging and multi-user support.",
+        tech: ["React", "Node.js", "Socket.IO","MongoDB"],
+        Links:"Live: https://vibechat-frontend.vercel.app/",
+        github: "Github Link : https://github.com/archi-cell/vibechat-frontend.git , https://github.com/archi-cell/vibechat-backend.git",
+        problem:
+            "Event planning involves multiple fragmented steps such as venue booking, catering, scheduling and payment, often handled on separate platforms.",
+
+        solution:
+            "Eventopia centralizes the entire event booking process into a single intuitive platform with real-time booking, cart and checkout features.",
+
+        features: [
+            "Venue & catering selection",
+            "Event calendar booking",
+            "Cart & checkout system",
+            "Live stream integration",
+            "Reviews & ratings",
+        ],
+    },
+    {
+        id: "institute",
+        title: "Institute Website",
+        description:
+            "Production-ready responsive website developed for an educational institute.",
+        tech: ["HTML", "CSS", "Javascript"],
+        Links:"Live:  https://nsdcomputer.com/",
+        github: "Github Link : https://github.com/archi-cell/nsd.git"
+    },
+];
+
+export default projects;
